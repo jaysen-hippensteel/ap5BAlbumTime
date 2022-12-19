321a9a747161d87195e293b4640a184fef35c2ed
Jaysen <jayhip16@gmail.com>
2022-12-19 09:25:50 -0500

    Initial Commit

---
+++ Album.java
@@ -0,0 +1,14 @@
+public class Album
+{
+  
+  //instance variables: remember to make them private and don't give them values yet (do that in the constructor)
+  
+  //constructor: accept the artist name and album title as parameters, set all other instance variables to default values
+
+  //mutator method: track instance variables
+
+  //toString method: return album summart
+
+  //accessor methods: return the instance variables
+
+}
---
+++ Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) 
+  {
+    System.out.println("Welcome to AlbumTime! Enter the name of the first artist.");
+    Scanner scan = new Scanner(System.in);
+    String artist = scan.nextLine();
+    System.out.println("\n\nAnd now, the first album title.");
+    String title = scan.nextLine();
+    
+    //begin by declaring your first album object here
+
+
+    //ask how many songs are on the album next, then make a loop to get the inputs
+
+
+  }
+}
