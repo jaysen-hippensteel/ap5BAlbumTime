44a3f998187b008c1c896d306168e77bdb35ee57
Jaysen <jayhip16@gmail.com>
2022-12-19 10:35:04 -0500

    Changes by nathan_d_merlo

--- Album.java
+++ Album.java
@@ -2,12 +2,17 @@ public class Album
 {
   
   //instance variables: remember to make them private and don't give them values yet (do that in the constructor)
-  
+  private String name;
+  private String albumbs;
+  priavte int numSongs;
+  priavte int minlength;
+  private int maxlength;
+  private int totallength;
   //constructor: accept the artist name and album title as parameters, set all other instance variables to default values
 
   //mutator method: track instance variables
 
-  //toString method: return album summart
+  //toString method: return album summary
 
   //accessor methods: return the instance variables
 
