ded7313c136a52be0edfded762bf0c7a400af58c
Jaysen <jayhip16@gmail.com>
2022-12-19 10:34:51 -0500

    My Commits

--- Main.java
+++ Main.java
@@ -3,14 +3,24 @@ import java.util.Scanner;
 class Main {
   public static void main(String[] args) 
   {
-    System.out.println("Welcome to AlbumTime! Enter the name of the first artist.");
-    Scanner scan = new Scanner(System.in);
-    String artist = scan.nextLine();
-    System.out.println("\n\nAnd now, the first album title.");
-    String title = scan.nextLine();
-    
-    //begin by declaring your first album object here
+    for (int k=0;k<2;k++){
+      System.out.println("Welcome to AlbumTime! Enter the name of the artist.");
+      Scanner scan = new Scanner(System.in);
+      String artist = scan.nextLine();
+      System.out.println("\n\nAnd now, the album title.");
+      String title = scan.nextLine();
+      System.out.println("\n\nHow many songs on the album? ");
+      int songs = scan.nextInt();
+      for (int c=0; c<songs; c++){
+        System.out.println("Length of song " + c + ": \n<Minutes: ");
+        int minutes = scan.nextInt();
+        System.out.println("\n<Seconds: ");
+        int seconds = scan.nextInt();
 
+      }
+
+    //begin by declaring your first album object here
+    //TODO
 
     //ask how many songs are on the album next, then make a loop to get the inputs
 
