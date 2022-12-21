dc583697941d0c8766e5889d7f14050e785b034d
Jaysen <jayhip16@gmail.com>
2022-12-21 11:16:02 -0500

    12/21/22 Commits

--- Album.java
+++ Album.java
@@ -1,15 +1,38 @@
+import java.util.ArrayList; // import the ArrayList class
+import java.util.Collections;//import Collections
+
 public class Album
 {
   
   //instance variables: remember to make them private and don't give them values yet (do that in the constructor)
   private String name;
-  private String albumbs;
-  priavte int numSongs;
-  priavte int minlength;
-  private int maxlength;
-  private int totallength;
+  private String albums;
+  private Integer numSongs;
+  private Integer minlength;
+  private Integer maxlength;
+  private Integer avgLength;
+  private Integer totalLength;
+  private ArrayList<Integer> songData = new ArrayList<Integer>(); // Create an ArrayList object
   //constructor: accept the artist name and album title as parameters, set all other instance variables to default values
+  public Album(String nm, String alb, Integer nmsngs){
+    name = nm;
+    albums = alb;
+    numSongs = nmsngs;
+  }
 
+  public void addLengths(Integer secs){
+    songData.add(secs);
+  }
+
+  public String toString(){
+    ArrayList<Integer> findMin = songData;
+    Collections.sort(findMin);
+    minlength = findMin.get(0);
+    //TODO get max, average, and totalLength to return, and compare
+
+    String returner = "Artist: " + name + "\nAlbum: " + albums + "\nSongs: " + numSongs.toString() + "\nMin Length: " + minlength.toString() + "\nMax Length" + maxlength.toString() + "\nAverage Length: " + avgLength.toString() + "\nTotal Length: " + totalLength.toString();
+    return returner;
+  }
   //mutator method: track instance variables
 
   //toString method: return album summary
--- Main.java
+++ Main.java
@@ -1,29 +1,59 @@
 import java.util.Scanner;
 
 class Main {
-  public static void main(String[] args) 
-  {
-    for (int k=0;k<2;k++){
-      System.out.println("Welcome to AlbumTime! Enter the name of the artist.");
+  public static void main(String[] args) {
+    Album albumOne;
+    Album albumTwo;
+    String w;
+    System.out.println("Welcome to AlbumTime!");
+    for (int k = 0; k < 2; k++) {
+      System.out.println("Enter the name of the artist.");
       Scanner scan = new Scanner(System.in);
       String artist = scan.nextLine();
       System.out.println("\n\nAnd now, the album title.");
       String title = scan.nextLine();
+
+      //Songs and their lengths
       System.out.println("\n\nHow many songs on the album? ");
       int songs = scan.nextInt();
-      for (int c=0; c<songs; c++){
-        System.out.println("Length of song " + c + ": \n<Minutes: ");
-        int minutes = scan.nextInt();
-        System.out.println("\n<Seconds: ");
-        int seconds = scan.nextInt();
+      if (k == 0) {
+        albumOne = new Album(artist, title, songs);
+        for (int c = 0; c < songs; c++) {
+          System.out.println("Length of song " + c + ": \n<Minutes: ");
+          int minutes = scan.nextInt();
+          w = scan.nextLine();
+          System.out.println("\n<Seconds: ");
+          int seconds = scan.nextInt();
+          w = scan.nextLine();
 
+          seconds += minutes * 60;
+          albumOne.addLengths(seconds);
+        }
+      }
+      else {
+        albumTwo = new Album(artist, title, songs);
+        for (int c = 0; c < songs; c++) {
+          System.out.println("Length of song " + c + ": \n<Minutes: ");
+          int minutes = scan.nextInt();
+          w = scan.nextLine();
+          System.out.println("\n<Seconds: ");
+          int seconds = scan.nextInt();
+          w = scan.nextLine();
+
+          seconds += minutes * 60;
+          albumTwo.addLengths(seconds);
+        }
+        }
       }
 
-    //begin by declaring your first album object here
-    //TODO
-
-    //ask how many songs are on the album next, then make a loop to get the inputs
 
 
+      //begin by declaring your first album object here
+      //TODO make array for songs minutes and seconds
+
+      //ask how many songs are on the album next, then make a loop to get the inputs
+
+
+    }
   }
 }
